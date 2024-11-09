dce0b668cf0c6701bbdefc6a47fad7a4e0f978d6
Andrew Njoo <adnjoo@gmail.com>
2024-11-09 06:52:02 -0600

    description

--- web/app/(landing)/page.tsx
+++ web/app/(landing)/page.tsx
@@ -10,8 +10,11 @@ export type Example = {
   video_id: string;
 };
 
-async function getData(): Promise<Example[]> {
+async function getData(): Promise<any> {
   const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
   // Fetch the latest 5 summaries (only URLs) from Supabase
   const { data, error } = await supabase
@@ -24,20 +27,35 @@ async function getData(): Promise<Example[]> {
     console.error('Error fetching summaries:', error);
     return [];
   }
-
-  return data.map(({ video_id, title }) => ({
+  // Map the data to include URLs and thumbnails
+  const mappedData = data.map(({ video_id, title }) => ({
     url: getYouTubeURL(video_id),
     thumbnail: getThumbnail(video_id),
     title,
     video_id,
   }));
+
+  return {
+    user,
+    data: mappedData,
+  };
 }
 
 export default async function Home() {
-  const data = await getData();
+  const { data, user } = await getData();
   return (
     <main className='flex flex-col py-4 sm:py-24'>
       <h1 className='text-center text-4xl font-bold'>SummaTube</h1>
+
+      {!user && (
+        <>
+          <p className='mx-auto mt-4 max-w-2xl text-center text-lg'>
+            SummaTube helps users to quickly access summaries of trending
+            YouTube videos, making it easy to stay informed.
+          </p>
+        </>
+      )}
+
       <Suspense fallback={null}>
         <LandingBody examples={data} />
       </Suspense>
