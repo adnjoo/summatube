c3d2e577d9d3dc4176b31b65bcafb8f87dd747b5
Andrew Njoo <adnjoo@gmail.com>
2024-11-11 02:36:46 -0600

    go to link (#119)

--- web/lib/helpers/getYouTubeURL.ts
+++ web/lib/helpers/getYouTubeURL.ts
@@ -1,3 +1,17 @@
-export const getYouTubeURL = (video_id: string): string =>
-    `https://www.youtube.com/watch?v=${video_id}`;
-  
+type GetYouTubeURLParams = {
+  video_id: string;
+  embed?: boolean;
+};
+
+/**
+ * Generates a YouTube URL based on the provided parameters.
+ * @param {GetYouTubeURLParams} params - The parameters to generate the URL.
+ * @returns {string} The generated YouTube URL.
+ */
+export const getYouTubeURL = ({
+  video_id,
+  embed = false,
+}: GetYouTubeURLParams): string =>
+  embed
+    ? `https://www.youtube.com/embed/${video_id}`
+    : `https://www.youtube.com/watch?v=${video_id}`;
