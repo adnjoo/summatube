c7c34454a9f073b6d789cc926a08c128eda8f07d
Andrew Njoo <adnjoo@gmail.com>
2024-11-11 18:51:27 -0600

    MC

--- web/app/(landing)/LandingBody.tsx
+++ web/app/(landing)/LandingBody.tsx
@@ -6,7 +6,8 @@ import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 import { type Example } from '@/app/(landing)/page';
-import { Marquee } from '@/components/Marquee';
+import { Marquee } from '@/components/layout/Marquee';
+import { MarqueeCard } from '@/components/MarqueeCard';
 import { SummaryCard } from '@/components/SummaryCard';
 import { Button, Input, Switch } from '@/components/ui';
 import {
@@ -145,22 +146,11 @@ export default function LandingBody({ examples }: { examples: Example[] }) {
       {showExamples && (
         <Marquee>
           {examples.map((example: Example) => (
-            <div
-              key={example.video_id + example.id}
-              className='group relative cursor-pointer'
-              onClick={() =>
-                handleThumbnailClick(example.video_id, example.title)
-              }
-            >
-              <img
-                className='z-50 max-w-[120px] cursor-pointer rounded-sm shadow-md sm:max-w-[180px]'
-                src={example.thumbnail}
-                alt='thumbnail'
-              />
-              <div className='absolute bottom-0 left-0 z-0 w-full bg-black bg-opacity-75 p-1 text-center text-xs text-white opacity-0 transition-opacity group-hover:opacity-100'>
-                {example.title}
-              </div>
-            </div>
+            <MarqueeCard
+              key={example.video_id}
+              example={example}
+              handleThumbnailClick={handleThumbnailClick}
+            />
           ))}
         </Marquee>
       )}
