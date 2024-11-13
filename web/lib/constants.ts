70a2dd2bf7f91c50519759ebec840b8a957b53ef
Andrew Njoo <adnjoo@gmail.com>
2024-11-13 06:17:13 -0600

    s_u

--- web/lib/constants.ts
+++ web/lib/constants.ts
@@ -6,7 +6,7 @@ export const AppConfig = {
     DESCRIPTION: 'Summarize YouTube videos for free with summa.tube. Quickly generate summaries for any video, saving you time and helping you capture key points instantly.',
   },
   CONTACT_EMAIL: 'support@summa.tube',
-  SITE_URL: 'https://summa.tube',
+  SITE_URL: 'https://www.summa.tube',
   SITE_MAP: {
     HOME: '/',
     PRIVACY: '/policies/privacy',
