021070cc765cd0d2570e1d847156b633c2516dcb
Andrew Njoo <adnjoo@gmail.com>
2024-11-13 16:47:56 -0600

    minimal

--- web/db/20241110150742_add_stripe.sql
+++ web/db/20241110150742_add_stripe.sql
@@ -1,45 +1,11 @@
--- Minimal Users table
+-- Users table
 CREATE TABLE public.users (
     id UUID REFERENCES auth.users(id) PRIMARY KEY,
     full_name TEXT,
     avatar_url TEXT,
-    stripe_customer_id TEXT  -- Stores Stripe customer ID directly in users table
+    stripe_customer_id TEXT,
+    pro BOOLEAN DEFAULT FALSE
 );
-
 ALTER TABLE public.users ENABLE ROW LEVEL SECURITY;
 CREATE POLICY "Can view own data" ON public.users FOR SELECT USING (auth.uid() = id);
 CREATE POLICY "Can update own data" ON public.users FOR UPDATE USING (auth.uid() = id);
-
--- Minimal Products table
-CREATE TABLE public.products (
-    id TEXT PRIMARY KEY,
-    active BOOLEAN,
-    name TEXT
-);
-ALTER TABLE public.products ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "Allow public read-only access" ON public.products FOR SELECT USING (TRUE);
-
--- Minimal Prices table
-CREATE TABLE public.prices (
-    id TEXT PRIMARY KEY,
-    product_id TEXT REFERENCES public.products,
-    active BOOLEAN,
-    unit_amount BIGINT,
-    currency TEXT CHECK (char_length(currency) = 3)
-);
-ALTER TABLE public.prices ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "Allow public read-only access" ON public.prices FOR SELECT USING (TRUE);
-
--- Minimal Subscriptions table
-CREATE TABLE public.subscriptions (
-    id TEXT PRIMARY KEY,
-    user_id UUID REFERENCES auth.users(id) NOT NULL,
-    status TEXT,
-    price_id TEXT REFERENCES public.prices
-);
-ALTER TABLE public.subscriptions ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "Can view own subs data" ON public.subscriptions FOR SELECT USING (auth.uid() = user_id);
-
--- Minimal real-time publication for products and prices
--- DROP PUBLICATION IF EXISTS supabase_realtime;
--- CREATE PUBLICATION supabase_realtime FOR TABLE public.products, public.prices;
