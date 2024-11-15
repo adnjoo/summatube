35a18b10d144913a99a801be8eb7e565db34cffe
Andrew Njoo <adnjoo@gmail.com>
2024-11-15 17:59:41 -0600

    mv

--- web/app/api/stripe-webhook/route.ts
+++ web/app/api/stripe-webhook/route.ts
@@ -1,7 +1,12 @@
 // app/api/stripe-webhook/route.ts
+import { createClient } from '@supabase/supabase-js';
 import Stripe from 'stripe';
 
-import { supabaseAdmin } from '@/utils/supabase/supabaseAdmin';
+// Note: supabaseAdmin uses the SERVICE_ROLE_KEY which you must only use in a secure server-side context
+const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+);
 
 // import {
 //   deletePriceRecord,
