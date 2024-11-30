5ecbb0864598b607dfceebf29f2b91bd2ffd6c1c
Andrew Njoo <adnjoo@gmail.com>
2024-11-30 08:52:36 -0600

    ext: inconsistent loading (#199)

--- extension/src/pages/background/index.ts
+++ extension/src/pages/background/index.ts
@@ -1,1 +1,11 @@
-console.log('background script loaded123');
+// Listen for navigation events on YouTube
+chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+  if (details.url && details.url.includes('youtube.com/watch')) {
+    console.log('Navigation detected:', details.url);
+    // Send a message to the content script to trigger initialization
+    chrome.tabs.sendMessage(details.tabId!, { action: 'initialize' });
+  }
+});
+
+// Enable service worker in the background script (required for Manifest V3)
+export {};
